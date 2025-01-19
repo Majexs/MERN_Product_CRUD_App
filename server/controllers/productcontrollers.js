52d52a5c62dabe6613926b240b9181718a8eb867
Majexs <166327239+Majexs@users.noreply.github.com>
2025-01-18 19:56:04 -0500

    began building out client side

--- server/controllers/productcontrollers.js
+++ server/controllers/productcontrollers.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-import Product from "../models/products";
+import Product from "../models/products.js";
 
-export const getProducts = async (req, res) => {
+export const createProduct = async (req, res) => {
     const product = req.body;
 
     if (!product.name || !product.price || ! product.image){
@@ -19,7 +19,7 @@ export const getProducts = async (req, res) => {
     }
 };
 
-export const createProduct = async (req, res) => {
+export const getProducts = async (req, res) => {
     try{
         const products = await Product.find({});
         res.status(200).json({success: true, data: products});
@@ -47,11 +47,16 @@ export const updateProduct = async (req, res) => {
 
 export const deleteProduct = async(req, res) => {
     const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message: "Invalid Product Id"});
+    }
+
     try{
         await Product.findByIdAndDelete(id);
         res.status(200).json({success: true, message: "Product deleted"});
     } catch (error){
         console.log("Error in deleting product", error.message);
-        res.status(404).json({success: false, message: "Product not found"});
+        res.status(500).json({success: false, message: "Server Error"});
     }
 };
--- server/server.js
+++ server/server.js
@@ -6,12 +6,13 @@ import productRoutes from './routes/productroutes.js'
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.use(express.json());
 app.use("/api/products", productRoutes)
 
 app.listen(5000, () => {
     connectDB();
-    console.log("Server started at http://localhost:5000");
+    console.log("Server started at http://localhost:"+PORT);
 
 });
